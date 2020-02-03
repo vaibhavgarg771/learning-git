b1a86652a6e64b527b9d1c9df70723c0050142ee
Vaibhav Garg <v.garg@salesforce.com>
2020-02-04 00:02:35 +0530

    making an insertion

--- manage.py
+++ manage.py
@@ -10,7 +10,7 @@ def main():
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "C. Are you sure it's installed and "
+            "Some insertions here. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
