328e35a08b9b5d9a2093469d8332a59afe5c3070
Vaibhav Garg <v.garg@salesforce.com>
2020-02-04 00:08:27 +0530

    nested gitignore

--- player/urls.py
+++ player/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     re_path(r'login$', LoginView.as_view(template_name = 'player/login_form.html'), name = 'player_login'),
     re_path(r'logout$', LogoutView.as_view(), name = 'player_logout'),
     re_path(r'home/$', home, name = 'player_home'),
+    #anyRandom stuff
 ]
 
